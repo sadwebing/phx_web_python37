5d1132fa60d5773e16726bcd235a1c5f54304259
root <root@PUB_SM_HK_62_91.(none)>
2018-01-22 23:52:32 +0800

    install mysql and startapp accounts

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 20170709 django version 1.8.0
 20170709 web: H5 CSS JS BOOTSTRAP3 BOOTSTRAP-TABLE BOOTSTRAP-DATETIMEPICKER BOOTSTRAP-SELECT KNOCKOUT TOASTR TITATOGGLE-DIST SUI.NAV 
+20180122 startapp accounts | yum install sqlite-devel | pip install pysqlite | wget http://www.sqlite.org/snapshot/sqlite-snapshot-201801181900.tar.gz |./configure && make && make install |cd /usr/local/src/Python-2.7.14 && make && make install
+20180122 yum install mysql-devel |pip install mysql-python
+
+
+
--- phxweb/settings.py
+++ phxweb/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -76,8 +77,18 @@ WSGI_APPLICATION = 'phxweb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'phxweb',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': 'phxweb',
+        'PASSWORD': 'phexus666',
+        'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '3306',                      # Set to empty string for default.
+        'OPTIONS': {
+            #'init_command': 'SET sql_mode=STRICT_TRANS_TABLES',
+            'charset': 'utf8',
+        },
     }
 }
 
