be0c27afc43f5b3e67645b20de68f97afff7e7ec
arno <arnode125@gmail.com>
2018-05-23 21:42:28 +0800

    upgrade sendTelegram

--- detect/telegram.py
+++ detect/telegram.py
@@ -31,27 +31,56 @@ class sendTelegram(object):
         bot        = message['bot'] if message.has_key('bot') else ''
         doc        = message['doc'] if message.has_key('doc') else False
         group      = message['group'] if message.has_key('group') else ''
-        parse_mode = message['parse_mode'] if message.has_key('parse_mode') else 'HTML'
         timeout    = message['timeout'] if message.has_key('timeout') and isinstance(message['timeout'], int) else 15
 
         self.__message = {}
         self.__doc     = doc
         self.__timeout = timeout
         self.__url     = tg['url'][bot] if tg['url'].has_key(bot) else tg['url']['sa_monitor_bot']
-        self.__message['chat_id']    = tg['chat_id'][group] if tg['chat_id'].has_key(group) else tg['chat_id']['arno_test']
-        self.__message['parse_mode'] = parse_mode
-        self.__message['text']       = message['text'] if message.has_key('text') else ''
+        self.__message['chat_id']    = tg['chat_id'][group]  if tg['chat_id'].has_key(group)  else tg['chat_id']['arno_test']
+        self.__message['parse_mode'] = message['parse_mode'] if message.has_key('parse_mode') else ''
+        self.__message['doc_name']   = message['doc_name']   if message.has_key('doc_name')   else 'warning.txt'
+        self.__message['text']       = self.getAtUsers(message['text']) if message.has_key('text') else ''
+
+    def getAtUsers(self, text):
+        user_l = [ {'user': '@'+re.match('[a-z|A-Z]+(?![a-z])', user).group(), 
+                    'name': re.match('[a-z]+(?![a-z])', user.lower()).group()} 
+                    for user in text.split('@')[1:] if re.match('[a-z]+(?![a-z])', user.lower())]
+
+        #logger.info(user_l)
+
+        if self.__message['parse_mode'] == 'HTML' or self.__message['parse_mode'] == 'Markdown':
+            user_id_l = {}
+            s = telegram_user_id_t.objects.all()
+            for i in s:
+                user_id_l[i.user] = {}
+                user_id_l[i.user]['name']    = i.name
+                user_id_l[i.user]['user_id'] = i.user_id
+        #logger.info(user_id_l)
+        if self.__message['parse_mode'] == 'HTML':
+            for user in user_l:
+                if user_id_l.has_key(user['name']):
+                    #logger.info(user_id_l[user['name']])
+                    text = text.replace(user['user'], "<a href='tg://user?id=%s'>%s</a>" %(user_id_l[user['name']]['user_id'], user_id_l[user['name']]['name']))
+
+        if self.__message['parse_mode'] == 'Markdown':
+            for user in user_l:
+                if user_id_l.has_key(user['name']):
+                    text = text.replace(user['user'], "[%s](tg://user?id=%s)" %(user_id_l[user['name']]['name'], user_id_l[user['name']]['user_id']))
+        #logger.info(text)
+        return text
 
     def send(self):
         try:
-            if self.__doc:
-                with open('warning.txt', 'w') as f:
-                    for line in text.split('\n'):
-                        f.writelines(line+'\r\n')
-                self.__files = {'document': open('warning.txt', 'rb')}
-                ret = requests.post(self.__url+'sendDocument', data=self.__message, files=self.__files, timeout=self.__timeout)
-            else:
+            if (not self.__doc) or self.__doc.lower() == 'false':
                 ret = requests.post(self.__url+'sendMessage', data=self.__message, timeout=self.__timeout)
+            else:
+                with open(self.__message['doc_name'], 'w') as f:
+                    for line in self.__message['text'].split('\n'):
+                        f.writelines(line+'\r\n')
+                self.__files = {'document': open(self.__message['doc_name'], 'rb')}
+                ret = requests.post(self.__url+'sendDocument', data=self.__message, files=self.__files, timeout=self.__timeout)
+                
         except Exception, e:
             logger.error('Attention: send message failed!')
             logger.error(e.message)
